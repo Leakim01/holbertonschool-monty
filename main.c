cf6de10817e690bed63e8dd5597145db6ccc03e7
Leakim01 <5662@holbertonstudents.com>
2022-12-14 07:48:16 -0800

    Push Function

--- main.c
+++ main.c
@@ -2,14 +2,24 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
+    stack_t *new_node;
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+        return;
 
+    new_node->n = n_extern;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    if ((*stack) != NULL)
+        (*stack)->prev = new_node;
+    *stack = new_node;
 }
 
 int main(int argc, char **argv)
 {
     printf("%s\n", argv[1]);
-    FILE * fp;
-    char * line = NULL;
+    FILE *fp;
+    char *line = NULL;
     size_t len = 0;
     ssize_t read;
     char *command;
@@ -19,13 +29,16 @@ int main(int argc, char **argv)
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
-    while ((getline(&line, &len, fp)) != -1) {
+    while ((getline(&line, &len, fp)) != -1)
+    {
         command = strtok(line, " \t");
         printf("%s\n", command);
         value = strtok(NULL, " \t");
-        if(value)
-        ;
-            // push(?????);
+        if (value)
+            printf("%s\n", L<line_number>: usage: push integer)
+            exit (EXIT_FAILURE);
+
+        // push(?????);
         //     printf("%s\n", line);
     }
 
