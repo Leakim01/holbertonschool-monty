c521e1969bf1c2f39efa06dbf5c3e3961e490649
AymericLPR <mric31400@gmail.com>
2022-12-16 00:49:23 -0800

    update with the new flag

--- opcode.c
+++ opcode.c
@@ -12,6 +12,7 @@
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *node;
+	(void)line_number;
 
 	node = malloc(sizeof(stack_t));
 	if (node == NULL)
@@ -41,6 +42,7 @@ void push(stack_t **stack, unsigned int line_number)
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *newnode;
+	(void)line_number;
 
 	if (*stack == NULL)
 		return;
